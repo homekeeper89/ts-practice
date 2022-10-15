a29cc211c14121934880dcd51c780478eed7ac67
matthew <matthew@cupist.com>
2022-10-15 22:58:04 +0900

    [feat] 기본 구현

--- app/src/algorithm/hacker-rank.spec.ts
+++ app/src/algorithm/hacker-rank.spec.ts
@@ -1,13 +1,25 @@
 import { diagonalDifference } from "@app/algorithm/hacker-rank";
 
 describe("diagonal difference", () => {
-  it("should return expected", () => {
-    const data = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-    ];
+  it.each([
+    [
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+        [9, 8, 9],
+      ],
+      2,
+    ],
+    [
+      [
+        [11, 2, 4],
+        [4, 5, 6],
+        [10, 8, -12],
+      ],
+      15,
+    ],
+  ])("should return expected", (data, expectValue) => {
     const res = diagonalDifference(data);
-    expect(res).toEqual(2);
+    expect(res).toEqual(expectValue);
   });
 });
--- app/src/algorithm/hacker-rank.ts
+++ app/src/algorithm/hacker-rank.ts
@@ -2,5 +2,16 @@
  * @link https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
  */
 export function diagonalDifference(arr: number[][]): number {
-  return 0;
+  const getNumberByIndex = (index: number, array: number[]): number => {
+    return array[index];
+  };
+
+  let left = 0;
+  let right = 0;
+  arr.forEach((array, index) => {
+    left += getNumberByIndex(index, array);
+    right += getNumberByIndex(array.length - index - 1, array);
+  });
+
+  return Math.abs(left - right);
 }
